c2923bf2c52ef006d43cb249d1c311add671d03b
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-18 19:09:30 +0800

    mybatis自动生成工具的自定义修改

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SpringMvcByScalaInIDEA
 
-Spring整合mybits，利用mybits自动生成model，dao，mapper.xml
+Spring整合mybits，利用mybits自动生成model，dao，mapper.xml<br>
+<br>
+如果要使mybatis自动生成的实体类带数据库的字段注释可以参考[这里](https://github.com/xiyuan-fengyu/MybatisDemo)
--- sql/mvn.sql
+++ sql/mvn.sql
@@ -1,31 +1,54 @@
 /*
 Navicat MySQL Data Transfer
+
 Source Server         : localhost
 Source Server Version : 50621
 Source Host           : localhost:3306
 Source Database       : mvn
+
 Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
-Date: 2016-06-30 17:16:37
+
+Date: 2016-10-18 16:48:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_score
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_score`;
+CREATE TABLE `tb_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+  `sex` int(1) NOT NULL DEFAULT '0' COMMENT '0:男生，1女生',
+  `score` double(10,0) NOT NULL DEFAULT '0' COMMENT '成绩',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_score
+-- ----------------------------
+INSERT INTO `tb_score` VALUES ('1', 'Tom', '0', '10');
+INSERT INTO `tb_score` VALUES ('2', 'Cat', '0', '7');
+INSERT INTO `tb_score` VALUES ('3', 'Jucy', '1', '9');
+INSERT INTO `tb_score` VALUES ('4', 'Caselin', '1', '10');
 
 -- ----------------------------
 -- Table structure for tb_test
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_test`;
 CREATE TABLE `tb_test` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
+  `id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(255) NOT NULL COMMENT '名字',
+  `content` varchar(255) NOT NULL COMMENT '内容',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_test
 -- ----------------------------
-INSERT INTO `tb_test` VALUES ('1', '0', '000');
 INSERT INTO `tb_test` VALUES ('2', '1', '111');
 INSERT INTO `tb_test` VALUES ('4', 'newName', 'newContent');
+INSERT INTO `tb_test` VALUES ('6', 'new name', '1L');
