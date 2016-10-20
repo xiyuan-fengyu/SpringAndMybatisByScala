2a89ef0d7bfbc1a1ab9b02ae8aa55558d903557c
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-20 16:32:04 +0800

    添加service层，测事务在异常情况下回滚

--- src/main/scala/com/xiyuan/template/controller/TestController.scala
+++ src/main/scala/com/xiyuan/template/controller/TestController.scala
@@ -2,6 +2,8 @@ package com.xiyuan.template.controller
 
 import com.google.gson.{Gson, JsonObject}
 import com.xiyuan.template.dao.{TbTestDao, TbTestMapper}
+import com.xiyuan.template.log.XYLog
+import com.xiyuan.template.service.TestService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
 class TestController {
 
   @Autowired
-  private val tbTestDao: TbTestDao = null
+  private val testService: TestService = null
 
   @RequestMapping(value = Array("test"))
   @ResponseBody
@@ -28,12 +30,37 @@ class TestController {
   @ResponseBody
   def testList(): JsonObject = {
     val result = new JsonObject
-    val maxId = tbTestDao.maxId()
-    result.add("all", new Gson().toJsonTree(tbTestDao.idBetween(0, maxId)))
+    val maxId = testService.maxId()
+    result.add("all", new Gson().toJsonTree(testService.idBetween(0, maxId)))
     result.add("maxId", new Gson().toJsonTree(maxId))
-    result.add("idBetween", new Gson().toJsonTree(tbTestDao.idBetween(0, 10)))
+    result.add("idBetween", new Gson().toJsonTree(testService.idBetween(0, 10)))
     result.addProperty("success", true)
     result.addProperty("message", "test")
+    result
+  }
+
+  /**
+    * 测试事务回滚
+    *
+    * @return
+    */
+  @RequestMapping(value = Array("test/rollback"))
+  @ResponseBody
+  def rollback(): JsonObject = {
+    val result = new JsonObject
+
+    result.addProperty("before", XYLog.anyToString(testService.find(1)))
+    try {
+      testService.testRollback(1)
+    }
+    catch {
+      case e: Exception =>
+        result.addProperty("exception", e.getMessage)
+    }
+    result.addProperty("afterRollbackTest", XYLog.anyToString(testService.find(1)))
+
+    result.addProperty("success", true)
+    result.addProperty("message", "rollback test")
     result
   }
 
---
+++ src/main/scala/com/xiyuan/template/service/TestService.scala
@@ -0,0 +1,60 @@
+package com.xiyuan.template.service
+
+import com.xiyuan.template.dao.TbTestDao
+import com.xiyuan.template.model.TbTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+/**
+  * Created by xiyuan_fengyu on 2016/10/20.
+  */
+@Service
+@Transactional(rollbackFor = Array(classOf[Exception]))
+class TestService {
+
+  @Autowired
+  private val testDao: TbTestDao = null
+
+  def idBetween(start: Int, end: Int): Array[TbTest] = {
+    testDao.idBetween(start, end).toArray().map(_.asInstanceOf[TbTest])
+  }
+
+  def maxId(): Int = {
+    testDao.maxId()
+  }
+
+  def page(pageNum: Int, pageSize: Int): Array[TbTest] = {
+    testDao.page(pageNum, pageSize).toArray().map(_.asInstanceOf[TbTest])
+  }
+
+  def find(id: Long): TbTest = {
+    testDao.selectByPrimaryKey(id)
+  }
+
+  def testRollback(id: Long): Unit = {
+    val item = testDao.selectByPrimaryKey(id)
+    item.setName("newNameForRollbackTest")
+    item.setContent("newContentForRollbackTest")
+    testDao.updateByPrimaryKey(item)
+
+    //注意必须添加@Transactional(rollbackFor = Array(classOf[Exception]))注解才有效
+
+//    //主动抛出异常可以使事务回滚
+//    throw new Exception("An exception for rollback!")
+
+    //运行时异常也可以使事务回滚
+    val i = Integer.parseInt("fsdjk2342354dsf")
+
+//    //如果异常被捕获，没有向上抛出，则无法使事务回滚
+//    try {
+//      val i = Integer.parseInt("fsdjk2342354dsf")
+//    }
+//    catch {
+//      case e: Exception =>
+//        e.printStackTrace()
+//    }
+
+  }
+
+}
